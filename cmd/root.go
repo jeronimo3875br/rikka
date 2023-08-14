02a851ef35a06ab8900a42afbb43afba6a544d9c
jeronimo3875br <gschimidit998919812@gmail.com>
2023-08-13 22:56:29 -0300

    setup cli application with cobra library

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "rikka",
+	Short: "Automatic reload for any program.",
+	Long:  `"Rikka" is a program designed for detecting changes in folders and files within your application, and it also facilitates automatic restarting.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// ...
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 Gabriel Jeronimo dev.gjeronimo@gmail.com
+*/
+
+package main
+
+import "github.com/jeronimo3875br/rikka/cmd"
+
+func main() {
+	cmd.Execute()
+}
